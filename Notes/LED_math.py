f3e9be56b8bc2b5aed0cbe159ec4668d63640647
danielmtro <Daniel.Monteiro2205@gmail.com>
2024-08-26 13:51:52 +1000

    feat: added math for LED calculation

---
+++ Notes/LED_math.py
@@ -0,0 +1,13 @@
+from math import comb
+
+num_LED = 16
+
+# total number of possibilities
+total = 2**num_LED
+
+# function to work out percentage chance of having 'x' number of LED's on
+calc_percent = lambda x: comb(num_LED, x)/total * 100
+
+# print all possibilities
+for i in range(num_LED + 1):
+    print(f"{i} moles: {calc_percent(i):.2f}")
