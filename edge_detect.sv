83de83f3b5ac1e952794570fef765e57c328365d
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2024-08-28 10:31:49 +1000

    edge detection working for 18 bits

--- edge_detect.sv
+++ edge_detect.sv
@@ -4,10 +4,15 @@ module name #() (
 
     output[17:0] SW_edge_det
 );
-    logic[17:0] button_q0;
+    logic[17:0] button_current;
+    logic [17:0] button_prev;
+
     always_ff @(posedge clk) begin : edge_detect
-        button_q0 <= SW_pressed;
+        button_current <= SW_pressed;
+        button_prev <= button_current;        
     end : edge_detect
     
-    assign SW_edge_det = (SW_pressed > button_q0);
+    assign SW_edge_det = (button_prev ^ button_current);
+
+    // assign SW_edge_det = (SW_pressed > button_q0);
 endmodule
