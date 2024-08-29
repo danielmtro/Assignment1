657e6c2a78259d1179bcc905e73bb4f66e612c41
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2024-08-29 12:24:30 +1000

    Update game_logic_tester.sv

--- module-tester/game_logic_tester.sv
+++ module-tester/game_logic_tester.sv
@@ -1,3 +1,67 @@
 module game_logic_tester	(
-
+	input CLOCK2_50,
+	input [17:0] SW,
+	
+	output [17:0] LEDR,
+	
+	output [6:0] HEX0,
+	output [6:0] HEX1,
+	output [6:0] HEX2,
+	output [6:0] HEX3
 );
+
+	logic [17:0] SW_pressed;
+	logic clk;
+	assign clk = CLOCK2_50;
+	
+	logic [10:0] score;
+	initial score = 0;
+	
+	logic point_1;
+	
+	logic [17:0] ledr;
+	always_comb begin
+		ledr = 18'b000110001000010000; 
+	end
+	assign LEDR = ledr;
+	
+	 genvar i;
+    generate
+
+        for(i=0; i<18; i++) begin  : debounce_switches
+            debounce d_i (.clk(CLOCK2_50),
+                          .button(SW[i]),
+                          .button_pressed(SW_pressed[i])
+								  );
+        end : debounce_switches
+    endgenerate
+	 
+	 
+	 
+	 
+	 
+	 display display_1 (	.clk(clk),
+								.value(score),
+								.display0(HEX0),
+								.display1(HEX1),
+								.display2(HEX2),
+								.display3(HEX3)
+								);
+	 game_logic game_logic (	.clk(clk),
+										.SW_pressed(SW_pressed),
+										.ledr(ledr),
+										.point_1(point_1)
+									);
+	 
+	 always_ff @(posedge clk) begin
+		if (point_1) begin
+			score <= score + 1;
+		end
+		else begin 
+			score <= score;
+		end
+			
+	 end 
+	 
+endmodule
+	 
